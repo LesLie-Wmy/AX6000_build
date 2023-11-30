6c36ac5b0977f07a23e07ed8966d0bc5d44b6815
LesLie-Wmy <133620294+LesLie-Wmy@users.noreply.github.com>
2023-12-01 00:22:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,7 @@
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
+echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
 #cd ./package
 
 #export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
