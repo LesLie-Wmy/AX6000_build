01f6668d11f5abfbf0be8848268c3f55b783dc73
LesLie-Wmy <133620294+LesLie-Wmy@users.noreply.github.com>
2024-03-13 21:13:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+
+source $GITHUB_WORKSPACE/functions.sh
+
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
