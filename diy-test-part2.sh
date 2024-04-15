8a452d233378fc2b73800907dc10b98463f5e795
LesLie-Wmy <133620294+LesLie-Wmy@users.noreply.github.com>
2024-04-15 17:01:02 +0800

    Update diy-test-part2.sh

--- diy-test-part2.sh
+++ diy-test-part2.sh
@@ -1,2 +1,6 @@
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang
-svn export https://github.com/immortalwrt/packages/trunk/lang/golang/ $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang
+# rm -rf $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang
+# svn export https://github.com/immortalwrt/packages/trunk/lang/golang/ $GITHUB_WORKSPACE/openwrt/feeds/packages/lang/golang
+# 更新 golang 1.22 版本
+cd $GITHUB_WORKSPACE/openwrt
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
