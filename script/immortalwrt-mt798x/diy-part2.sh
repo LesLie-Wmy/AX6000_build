86b57fb04317ae3ea48b539664246d5b80330d47
LesLie-Wmy <133620294+LesLie-Wmy@users.noreply.github.com>
2024-06-17 19:30:04 +0800

    Update diy-part2.sh

--- script/immortalwrt-mt798x/diy-part2.sh
+++ script/immortalwrt-mt798x/diy-part2.sh
@@ -120,8 +120,8 @@ rm -rf luci-app-passwall
 # 更新 golang 1.22 版本
 cd $GITHUB_WORKSPACE/openwrt
 rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
 # 更新 chinadns-ng 版本
 #svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/chinadns-ng/ $GITHUB_WORKSPACE/openwrt/feeds/jell/chinadns-ng
