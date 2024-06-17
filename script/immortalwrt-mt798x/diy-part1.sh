9b011b3822bf2a61a151aa2792d1a3dc294e3c7a
LesLie-Wmy <133620294+LesLie-Wmy@users.noreply.github.com>
2024-06-17 12:04:04 +0800

    Update diy-part1.sh

--- script/immortalwrt-mt798x/diy-part1.sh
+++ script/immortalwrt-mt798x/diy-part1.sh
@@ -19,7 +19,7 @@ source $GITHUB_WORKSPACE/functions.sh
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
